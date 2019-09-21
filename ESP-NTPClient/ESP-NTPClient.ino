8a4757c52aa76325be768797691be4ba6f832963
Janis <jan1s.coding@gmail.com>
2019-09-21 22:27:20 +0200

    Unified ESP8266 firmware, removed project-specific configuration.

--- ESP-NTPClient/ESP-NTPClient.ino
+++ ESP-NTPClient/ESP-NTPClient.ino
@@ -1,45 +1,67 @@
-#include <NTPClient.h>
 #include <ESP8266WiFi.h>
+
+#include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <ctime>
 
-const char *ssid     = "Nostromo";
-const char *password = "P1mm3lk0pf";
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+#include <Ticker.h>
+Ticker ticker;
 
-// Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-void setup(){
+void tick()
+{
+  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
+  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+}
+
+void configModeCallback (WiFiManager *myWiFiManager) 
+{
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  ticker.attach(0.2, tick);
+}
+
+void setup()
+{
   Serial.begin(115200);
 
-  WiFi.begin(ssid, password);
-
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    //Serial.print ( "." );
+  WiFiManager wifiManager;
+  wifiManager.setAPCallback(configModeCallback);
+  if (!wifiManager.autoConnect("AutoConnectAP", "password")) 
+  {
+    ESP.reset();
+    delay(1000);
   }
+
+  ticker.detach();
+  digitalWrite(BUILTIN_LED, LOW);
 
   timeClient.begin();
 }
 
-void loop() {
-  timeClient.update();
+void loop()
+{  
+  if(timeClient.update())
+  {
+    uint32_t secsSince1970 = timeClient.getEpochTime();
+    time_t epoch = secsSince1970;
+    tm *t = localtime(&epoch);
+  
+    Serial.printf("\r\nrtc_write %u %u %u %u %u %u\r\n", (unsigned int) t->tm_year + 1900, (unsigned int) t->tm_mon + 1, (unsigned int) t->tm_mday, (unsigned int) t->tm_hour, (unsigned int) t->tm_min, (unsigned int) t->tm_sec);
+    Serial.flush();
+  
+    digitalWrite(5, HIGH);
+    delay(1000);
 
-  uint32_t secsSince1970 = timeClient.getEpochTime();
-  time_t epoch = secsSince1970;
-  tm *t = localtime(&epoch);
-
-  Serial.print("\r\n");
-  Serial.flush();
-  Serial.printf ("rtc_write %u %u %u %u %u %u\r\n", (unsigned int) t->tm_year + 1900, (unsigned int) t->tm_mon + 1, (unsigned int) t->tm_mday, (unsigned int) t->tm_hour, (unsigned int) t->tm_min, (unsigned int) t->tm_sec);
-  Serial.flush();
-
-  digitalWrite(5, HIGH);
-
+    ESP.deepSleep(3.6e9); // deep-sleep for an hour
+  }
+  
   delay(1000);
-
-  ESP.deepSleep(3.6e9); // deep-sleep for an hour
 }
--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# fw-clock-esp (WORK IN PROGRESS)
-Firmware for the ESP8266 WiFi Adapter for the Clock Projects
+# fw-clock-esp
+Firmware for the ESP8266 WiFi adapter for the clock projects
 
-# LICENSE
+## Dependencies
 
-This project is derived from the ESP-WordClock project by Frank Meyer.
+The following dependencies should be installed using the Arduino library management:
 
-ESP-WordClock.ino - some ESP8266 network routines with communication interface via UART to WordClock (STM32)
- 
-Copyright (c) 2016 Frank Meyer - frank(at)fli4l.de
-             modified by jan1s - jan1s.cod1ng(at)gmail.com
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+https://github.com/arduino-libraries/NTPClient
+https://github.com/tzapu/WiFiManager
